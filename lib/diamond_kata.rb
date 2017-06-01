0b474786f2be97eb368e18f8a2ff4ed48464c959
Brett Heenan <brettrheenan@gmail.com>
2017-06-01 17:54:00 +0200

    first commit

---
+++ lib/diamond_kata.rb
@@ -0,0 +1,5 @@
+require "diamond_kata/version"
+
+module DiamondKata
+  # Your code goes here...
+end
---
+++ spec/diamond_kata_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe DiamondKata do
+  it "has a version number" do
+    expect(DiamondKata::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
